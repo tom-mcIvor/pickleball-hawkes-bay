0d17238d8971fcfe3c8e2725d9bb935c08d52441
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2023-10-31 01:16:06 +1300

    add styling to the html

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,77 +1,54 @@
 'use client'
 import { Typography } from "@material-tailwind/react";
  
-const SITEMAP = [
-  {
-    title: "Links",
-    links: ["Pickleball Brackes", "PGA", "Facebook page", "The Shed pickleball"],
-  },
-  {
-    title: "Help Center",
-    links: ["Discord", "Twitter", "GitHub", "Contact Us"],
-  },
-  {
-    title: "Resources",
-    links: ["Blog", "Newsletter", "Free Products", "Affiliate Program"],
-  },
-  {
-    title: "Products",
-    links: ["Templates", "UI Kits", "Icons", "Mockups"],
-  },
-];
- 
-const currentYear = new Date().getFullYear();
- 
 function Footer() {
   return (
-    <footer className="relative w-full">
-      <div className="mx-auto w-full max-w-7xl px-8">
-        <div className="mx-auto grid w-full grid-cols-1 gap-8 py-12 md:grid-cols-2 lg:grid-cols-4">
-          {SITEMAP.map(({ title, links }, key) => (
-            <div key={key} className="w-full">
-              <Typography
-                variant="small"
-                color="blue-gray"
-                className="mb-4 font-bold uppercase opacity-50"
-              >
-                {title}
-              </Typography>
-              <ul className="space-y-1">
-                {links.map((link, key) => (
-                  <Typography key={key} as="li" color="blue-gray" className="font-normal">
-                    <a
-                      href="#"
-                      className="inline-block py-1 pr-2 transition-transform hover:scale-105"
-                    >
-                      {link}
-                    </a>
-                  </Typography>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-        <div className="flex w-full flex-col items-center justify-center border-t border-blue-gray-50 py-4 md:flex-row md:justify-between">
+    <footer className="px-4 flex w-full flex-row flex-wrap items-center justify-center gap-y-6 gap-x-12 border-t border-blue-gray-50 py-6 text-center md:justify-between">
+      <Typography color="blue-gray" className="font-normal">
+        &copy; 2023 Pickleball HawkesBay
+      </Typography>
+      <ul className="flex flex-wrap items-center gap-y-2 gap-x-8">
+        <li>
           <Typography
-            variant="small"
-            className="mb-4 text-center font-normal text-blue-gray-900 md:mb-0"
+            as="a"
+            href="#"
+            color="blue-gray"
+            className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
           >
-            &copy; {currentYear} <a href="https://material-tailwind.com/">HawkesBay Pickleball</a>. All
-            Rights Reserved.
+            About Us
           </Typography>
-          <div className="flex gap-4 text-blue-gray-900 sm:justify-center">
-            <Typography as="a" href="https://www.facebook.com/groups/369141060184018" className="opacity-80 transition-opacity hover:opacity-100">
-              <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                <path
-                  fill-rule="evenodd"
-                  d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-            </Typography>
-          </div>
-        </div>
-      </div>
+        </li>
+        <li>
+          <Typography
+            as="a"
+            href="#"
+            color="blue-gray"
+            className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
+          >
+            License
+          </Typography>
+        </li>
+        <li>
+          <Typography
+            as="a"
+            href="#"
+            color="blue-gray"
+            className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
+          >
+            Contribute
+          </Typography>
+        </li>
+        <li>
+          <Typography
+            as="a"
+            href="#"
+            color="blue-gray"
+            className="font-normal transition-colors hover:text-blue-500 focus:text-blue-500"
+          >
+            Contact Us
+          </Typography>
+        </li>
+      </ul>
     </footer>
   );
 }
