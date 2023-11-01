33deb2943a8574bdae95e5155d3d45d96f1a8289
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2023-11-01 23:52:36 +1300

    add styling to the html

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import HomeContent from '@/components/HomeContent'
 export default function Home() {
   return ( 
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    
     <HomeContent/>
     </main>
   )
