c823676313bad0d25ca96c8aedfd94afd942e943
tommy-tommy-tom <105212576+tom-mcivor@users.noreply.github.com>
2023-11-01 20:40:59 +1300

    add styling to the html

--- src/components/HomeContent.tsx
+++ src/components/HomeContent.tsx
@@ -3,28 +3,18 @@ import Image from 'next/image'
 import React from 'react'
 import pickleBallCourts from '../../public/pickleballCourts.png'
 import garySwing from '../../public/GarySwing.png'
-import { Carousel } from "@material-tailwind/react"
+import { Carousel } from '@material-tailwind/react'
 
 function Home() {
   return (
     <>
-
-<div className="h-auto w-auto">
-  <Carousel className="rounded-xl">
-        <Image
-          src={garySwing}      
-          alt='garySwing'
-        />
-        <Image
-          src={pickleBallCourts}     
-          alt='pickleBallCourts'
-        />
-        <Image
-          src={garySwing}       
-          alt='garySwing'
-        />
-      </Carousel>
-</div>
+      <div className="h-auto w-auto">
+        <Carousel className="rounded-xl">
+          <Image src={garySwing} alt="garySwing" />
+          <Image src={pickleBallCourts} alt="pickleBallCourts" />
+          <Image src={garySwing} alt="garySwing" />
+        </Carousel>
+      </div>
     </>
   )
 }
